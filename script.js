766945e315f80a03d97afdce3dd76075c9561373
JacobGeorgeMathew <152091838+JacobGeorgeMathew@users.noreply.github.com>
2024-07-15 11:15:42 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,106 @@
+const cells = document.querySelectorAll(".cell");
+const statusText = document.querySelector("#statusText");
+const restartBtn = document.querySelector("#restartBtn");
+const winConditions = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+];
+let options = ["", "", "", "", "", "", "", "", ""];
+let currentPlayer = "X";
+let running = false;
+let flag = [];
+let car = false;
+
+initializeGame();
+
+function initializeGame(){
+    cells.forEach(cell => cell.addEventListener("click", cellClicked));
+    restartBtn.addEventListener("click", restartGame);
+    statusText.textContent = `${currentPlayer}'s turn`;
+    running = true;
+}
+function cellClicked(){
+    const cellIndex = this.getAttribute("cellIndex");
+
+    if(options[cellIndex] != "" || !running){
+        return;
+    }
+
+    updateCell(this, cellIndex);
+    checkWinner();
+}
+function updateCell(cell, index){
+    options[index] = currentPlayer;
+    cell.textContent = currentPlayer;
+}
+function changePlayer(){
+    if(currentPlayer === "X"){
+        currentPlayer = "O";
+        document.querySelector(".bg").style.left = "85px";
+    }
+    else{
+        currentPlayer = "X";
+        document.querySelector(".bg").style.left = "0px";
+    }
+   
+    statusText.textContent = `${currentPlayer}'s turn`;
+}
+function checkWinner(){
+    let roundWon = false;
+
+    for(let i = 0; i < winConditions.length; i++){
+        const condition = winConditions[i];
+        const cellA = options[condition[0]];
+        const cellB = options[condition[1]];
+        const cellC = options[condition[2]];
+
+        if(cellA == "" || cellB == "" || cellC == ""){
+            continue;
+        }
+        if(cellA == cellB && cellB == cellC){
+            roundWon = true;
+            flag = condition;
+            break;
+        }
+    }
+
+    if(roundWon){
+        statusText.textContent = `${currentPlayer} wins!`;
+        
+        for(let j=0; j<3; j++){
+            cells[flag[j]].style.backgroundColor = "#7df9ff";
+            cells[flag[j]].style.color = "#000";
+            car = true;
+           
+        }
+        running = false;
+    }
+    else if(!options.includes("")){
+        statusText.textContent = `Draw!`;
+        running = false;
+    }
+    else{
+        changePlayer();
+    }
+}
+function restartGame(){
+    currentPlayer = "X";
+    options = ["", "", "", "", "", "", "", "", ""];
+    statusText.textContent = `${currentPlayer}'s turn`;
+    cells.forEach(cell => cell.textContent = "");
+   
+    if(car){
+        for(let j=0; j<3; j++){
+            cells[flag[j]].style.backgroundColor = "#000";
+            cells[flag[j]].style.color = "#ffffff";
+        }
+    }
+    running = true;
+   
+}
